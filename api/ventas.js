7e9d9bd0d58a4835b773c28eed650b4ea2662e4a
Gllacuas <gllacuas@donsalazar.com>
2026-06-19 19:12:35 -0500

    Add Cache-Control header to respfix: add Cache-Control no-store to prevent API cachingonse

--- api/ventas.js
+++ api/ventas.js
@@ -7,6 +7,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   try {
